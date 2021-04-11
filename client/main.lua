803ac0cc6ada9b82b359cee7d299dc89d9ca8dc4
GhzGarage <qbcore@programmer.net>
2021-04-10 21:54:52 -0500

    Initial Commit

---
+++ client/main.lua
@@ -0,0 +1,198 @@
+QBCore = nil
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(10)
+        if QBCore == nil then
+            TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
+            Citizen.Wait(200)
+        end
+    end
+end)
+
+isLoggedIn = false
+local PlayerJob = {}
+
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded')
+AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
+    isLoggedIn = true
+    PlayerJob = QBCore.Functions.GetPlayerData().job
+
+    if PlayerJob.name == "reporter" then
+        local blip = AddBlipForCoord(Config.Locations["vehicle"].coords.x, Config.Locations["vehicle"].coords.y, Config.Locations["vehicle"].coords.z)
+        SetBlipSprite(blip, 225)
+        SetBlipDisplay(blip, 4)
+        SetBlipScale(blip, 0.6)
+        SetBlipAsShortRange(blip, true)
+        SetBlipColour(blip, 1)
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentSubstringPlayerName(Config.Locations["vehicle"].label)
+        EndTextCommandSetBlipName(blip)
+    end
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerUnload')
+AddEventHandler('QBCore:Client:OnPlayerUnload', function()
+    isLoggedIn = false
+end)
+
+RegisterNetEvent('QBCore:Client:OnJobUpdate')
+AddEventHandler('QBCore:Client:OnJobUpdate', function(JobInfo)
+    PlayerJob = JobInfo
+
+    if PlayerJob.name == "reporter" then
+        local blip = AddBlipForCoord(Config.Locations["vehicle"].coords.x, Config.Locations["vehicle"].coords.y, Config.Locations["vehicle"].coords.z)
+        SetBlipSprite(blip, 225)
+        SetBlipDisplay(blip, 4)
+        SetBlipScale(blip, 0.6)
+        SetBlipAsShortRange(blip, true)
+        SetBlipColour(blip, 1)
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentSubstringPlayerName(Config.Locations["vehicle"].label)
+        EndTextCommandSetBlipName(blip)
+    end
+end)
+
+Citizen.CreateThread(function()
+    -- local blip = AddBlipForCoord(Config.Locations["main"].coords.x, Config.Locations["main"].coords.y, Config.Locations["main"].coords.z)
+    -- SetBlipSprite(blip, 459)
+    -- SetBlipDisplay(blip, 4)
+    -- SetBlipScale(blip, 0.6)
+    -- SetBlipAsShortRange(blip, true)
+    -- SetBlipColour(blip, 1)
+    -- BeginTextCommandSetBlipName("STRING")
+    -- AddTextComponentSubstringPlayerName(Config.Locations["main"].label)
+    -- EndTextCommandSetBlipName(blip)
+    while true do 
+        Citizen.Wait(1)
+        if isLoggedIn and QBCore ~= nil then
+            local pos = GetEntityCoords(PlayerPedId())
+            if PlayerJob.name == "reporter" then
+                if #(pos - vector3(Config.Locations["vehicle"].coords.x, Config.Locations["vehicle"].coords.y, Config.Locations["vehicle"].coords.z)) < 10.0 then
+                    DrawMarker(2, Config.Locations["vehicle"].coords.x, Config.Locations["vehicle"].coords.y, Config.Locations["vehicle"].coords.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.2, 0.15, 200, 200, 200, 222, false, false, false, true, false, false, false)
+                    if #(pos - vector3(Config.Locations["vehicle"].coords.x, Config.Locations["vehicle"].coords.y, Config.Locations["vehicle"].coords.z)) < 1.5 then
+                        if IsPedInAnyVehicle(PlayerPedId(), false) then
+                            DrawText3D(Config.Locations["vehicle"].coords.x, Config.Locations["vehicle"].coords.y, Config.Locations["vehicle"].coords.z, "~g~E~w~ - Store the Vehicle")
+                        else
+                            DrawText3D(Config.Locations["vehicle"].coords.x, Config.Locations["vehicle"].coords.y, Config.Locations["vehicle"].coords.z, "~g~E~w~ - Vehicles")
+                        end
+                        if IsControlJustReleased(0, Keys["E"]) then
+                            if IsPedInAnyVehicle(PlayerPedId(), false) then
+                                DeleteVehicle(GetVehiclePedIsIn(PlayerPedId()))
+                            else
+                                MenuGarage()
+                                Menu.hidden = not Menu.hidden
+                            end
+                        end
+                        Menu.renderGUI()
+                    end 
+                end
+            else
+                Citizen.Wait(2500)
+            end
+        else
+            Citizen.Wait(2500)
+        end
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do 
+        Citizen.Wait(1)
+        local inRange = false
+        if isLoggedIn and QBCore ~= nil then
+            local pos = GetEntityCoords(PlayerPedId())
+            if #(pos - vector3(Config.Locations["main"].coords.x, Config.Locations["main"].coords.y, Config.Locations["main"].coords.z)) < 1.5 or #(pos - vector3(Config.Locations["inside"].coords.x, Config.Locations["inside"].coords.y, Config.Locations["inside"].coords.z)) < 1.5 then
+                inRange = true
+                if #(pos - vector3(Config.Locations["main"].coords.x, Config.Locations["main"].coords.y, Config.Locations["main"].coords.z)) < 1.5 then
+                    DrawText3D(Config.Locations["main"].coords.x, Config.Locations["main"].coords.y, Config.Locations["main"].coords.z, "~g~E~w~ - Enter")
+                    if IsControlJustReleased(0, Keys["E"]) then
+                        DoScreenFadeOut(500)
+                        while not IsScreenFadedOut() do
+                            Citizen.Wait(10)
+                        end
+    
+                        SetEntityCoords(PlayerPedId(), Config.Locations["inside"].coords.x, Config.Locations["inside"].coords.y, Config.Locations["inside"].coords.z, 0, 0, 0, false)
+                        SetEntityHeading(PlayerPedId(), Config.Locations["inside"].coords.h)
+    
+                        Citizen.Wait(100)
+    
+                        DoScreenFadeIn(1000)
+                    end
+                elseif #(pos - vector3(Config.Locations["inside"].coords.x, Config.Locations["inside"].coords.y, Config.Locations["inside"].coords.z)) < 1.5 then
+                    DrawText3D(Config.Locations["inside"].coords.x, Config.Locations["inside"].coords.y, Config.Locations["inside"].coords.z, "~g~E~w~ - Go outside")
+                    if IsControlJustReleased(0, Keys["E"]) then
+                        DoScreenFadeOut(500)
+                        while not IsScreenFadedOut() do
+                            Citizen.Wait(10)
+                        end
+    
+                        SetEntityCoords(PlayerPedId(), Config.Locations["outside"].coords.x, Config.Locations["outside"].coords.y, Config.Locations["outside"].coords.z, 0, 0, 0, false)
+                        SetEntityHeading(PlayerPedId(), Config.Locations["outside"].coords.h)
+    
+                        Citizen.Wait(100)
+    
+                        DoScreenFadeIn(1000)
+                    end
+                end 
+            end
+        end
+        if not inRange then
+            Citizen.Wait(2500)
+        end
+    end
+end)
+
+function MenuGarage()
+    ped = PlayerPedId();
+    MenuTitle = "Garage"
+    ClearMenu()
+    Menu.addButton("Vehicles", "VehicleList", nil)
+    Menu.addButton("Close Menu", "closeMenuFull", nil) 
+end
+
+function VehicleList(isDown)
+    ped = PlayerPedId();
+    MenuTitle = "Vehicles:"
+    ClearMenu()
+    for k, v in pairs(Config.Vehicles) do
+        Menu.addButton(Config.Vehicles[k], "TakeOutVehicle", k, "Garage", " Motor: 100%", " Body: 100%", " Fuel: 100%")
+    end
+        
+    Menu.addButton("Back", "MenuGarage",nil)
+end
+
+function TakeOutVehicle(vehicleInfo)
+    local coords = Config.Locations["vehicle"].coords
+    QBCore.Functions.SpawnVehicle(vehicleInfo, function(veh)
+        SetVehicleNumberPlateText(veh, "TOWR"..tostring(math.random(1000, 9999)))
+        SetEntityHeading(veh, coords.h)
+        exports['LegacyFuel']:SetFuel(veh, 100.0)
+        closeMenuFull()
+        TaskWarpPedIntoVehicle(PlayerPedId(), veh, -1)
+        TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(veh))
+        SetVehicleEngineOn(veh, true, true)
+        CurrentPlate = GetVehicleNumberPlateText(veh)
+    end, coords, true)
+end
+
+function closeMenuFull()
+    Menu.hidden = true
+    currentGarage = nil
+    ClearMenu()
+end
+
+function DrawText3D(x, y, z, text)
+	SetTextScale(0.35, 0.35)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 215)
+    SetTextEntry("STRING")
+    SetTextCentre(true)
+    AddTextComponentString(text)
+    SetDrawOrigin(x,y,z, 0)
+    DrawText(0.0, 0.0)
+    local factor = (string.len(text)) / 370
+    DrawRect(0.0, 0.0+0.0125, 0.017+ factor, 0.03, 0, 0, 0, 75)
+    ClearDrawOrigin()
+end
---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-NewsJob'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client/main.lua',
+    'client/camera.lua',
+    'client/gui.lua',
+}
+
+server_scripts {
+    'server/main.lua',
+    'config.lua',
+}
